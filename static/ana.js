84ef325c465cca657ace0d5e229c0b63d4e40059
bthuya <75073985+bthuyadizhi@users.noreply.github.com>
2020-12-05 02:07:20 +0000

    Add files via upload

---
+++ static/ana.js
@@ -0,0 +1,8 @@
+document.writeln("<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-149327911-1\"></script>");
+document.writeln("<script>");
+document.writeln("  window.dataLayer = window.dataLayer || [];");
+document.writeln("  function gtag(){dataLayer.push(arguments);}");
+document.writeln("  gtag(\'js\', new Date());");
+document.writeln("");
+document.writeln("  gtag(\'config\', \'UA-149327911-1\');");
+document.writeln("</script>");
